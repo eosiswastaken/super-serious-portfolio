b0753df9dd3d983d22d0249f98e6271462ebe6a0
Antoine Cerdan <antoinecerdan31@gmail.com>
2023-04-18 12:47:25 +0200

    background image and 4 spaces for cards, projects, contact and name

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+  </head>
+  <body>
+    <style>
+body, html {
+  height: 100%;
+}
+
+.parallax {
+  /* The image used */
+  background-image: url("./img/clouds_background.jpg");
+
+  /* Full height */
+  height: 100%;
+  width: 100vw;
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+
+.projects {
+    height: 100vh;
+    background-color: #363636;
+}
+
+
+.cards {
+    height: 100vh;
+    background-color: #262626;
+    
+}
+
+
+.contact {
+    height: 100vh;
+    background-color: #161616;
+    
+}
+    </style>
+    <div class="parallax">
+        Antoine Cerdan
+    </div>
+    
+    <div class="projects">
+        Projects
+    </div>
+
+    
+    <div class="cards">
+        Cards
+    </div>
+
+    
+    <div class="contact">
+        Contact
+    </div>
+
+  </body>
+</html>
